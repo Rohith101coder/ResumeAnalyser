b72c58a26fa94fc5d786e09d3a633259495cbdfe
Rohith101coder <rohithvadla07@gmail.com>
2026-01-16 00:49:30 +0530

    control layer designed

---
+++ src/main/java/com/example/resumeParser/controller/ResumeController.java
@@ -0,0 +1,24 @@
+package com.example.resumeParser.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.resumeParser.service.ResumeService;
+
+@RestController
+@RequestMapping("/api/resume")
+public class ResumeController {
+    private final ResumeService resumeService;
+
+    public ResumeController(ResumeService resumeService){
+        this.resumeService=resumeService;
+    }
+
+    public ResponseEntity<String> uploadResume(@RequestParam("file") MultipartFile file){
+        String extractedText=resumeService.parseResume(file);
+        return ResponseEntity.ok(extractedText);
+    }
+}
